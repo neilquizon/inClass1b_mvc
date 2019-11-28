293d5bd0a832b9ac5d37a2d62967a4b0fc0c8760
neilquizon <neil.quizon@gmail.com>
2019-11-28 10:14:39 -0800

    Add project files.

---
+++ inClass1b_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace inClass1b_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
